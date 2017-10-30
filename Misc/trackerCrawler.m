b1ffdafa6f4eba554ac67cc960741bc6c825e176
itskov <itskov@gmail.com>
2017-10-30 20:45:14 +0200

    Gui Modifications, and crawler bug fix.

--- Misc/trackerCrawler.m
+++ Misc/trackerCrawler.m
@@ -1,6 +1,6 @@
 function [ ] = trackerCrawler( sourceDirectory, targetDirectory )
     % First we're looking for movie files (*.mj2)
-    lister = FileLister(sourceDirectory,'*.mj2');
+    lister = FileLister(sourceDirectory,'*.avi');
     videoFiles = lister.allFiles();
     numberOfMovies = length(videoFiles);
     
